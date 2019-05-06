6cd97774b6d3faf115eadfe90477085ae2863374
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-05-06 19:39:30 -0300

    modulo completo

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,14 +1,25 @@
 'use strict'
 
+const Database = use('Database')
 const User = use('App/Models/User')
 
 class UserController {
-  async store ({ request }) {
+  async store ({ request, response }) {
     const data = request.only(['username', 'email', 'password'])
+    const addresses = request.input('addresses')
 
-    const user = await User.create(data)
+    const trx = await Database.beginTransaction()
 
-    return user
+    try {
+      const user = await User.create(data, trx)
+      await user.addresses().createMany(addresses, trx)
+
+      await trx.commit()
+      return response.status(201).send(user)
+    } catch (error) {
+      trx.rollback()
+      return response.status(500).send(error)
+    }
   }
 }
 
